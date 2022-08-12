6484d7757edc2632bfcce85665e2d7875f0a5da9
DeepaliPashte <deepalipashte888@gmail.com>
2022-08-12 13:46:08 +0530

    project added to central repository

---
+++ src/test/java/com/Deepali/SptingBootCrudWithRestful/SpringBootCrudWithRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Deepali.SptingBootCrudWithRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudWithRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
