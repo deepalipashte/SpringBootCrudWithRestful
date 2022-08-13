000e883e463252055a8a3a0d196a33899856e5fa
DeepaliPashte <deepalipashte888@gmail.com>
2022-08-13 16:02:09 +0530

    Servicelmpl

--- src/main/java/com/Deepali/SptingBootCrudWithRestful/service/Servicelmpl.java
+++ src/main/java/com/Deepali/SptingBootCrudWithRestful/service/Servicelmpl.java
@@ -2,23 +2,28 @@ package com.Deepali.SptingBootCrudWithRestful.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Deepali.SptingBootCrudWithRestful.dao.EmployeeDaol;
 import com.Deepali.SptingBootCrudWithRestful.model.Employee;
 
 @Service
 public class Servicelmpl implements Servicel {
-
+     @Autowired
+	private EmployeeDaol employeeDaol;
+	
+	
 	@Override
 	public int SaveEmployee(Employee employee) {
-		
-		return 0;
+		int save = employeeDaol.saveEmployeeDaol(employee);
+		return save;
 	}
 
 	@Override
 	public List<Employee> getallEmployee(Employee employee) {
-		
-		return null;
+		List<Employee> allEmployeeDaol = employeeDaol.getAllEmployeeDaol();
+		return allEmployeeDaol;
 	}
 
 }
